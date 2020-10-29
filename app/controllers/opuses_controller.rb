a56e5f0ab11a878c72b4117347164d51009f753e
tairakanno <tairakannotaira@gmail.com>
2020-10-29 14:47:19 +0900

    ユーザー管理機能実装

---
+++ app/controllers/opuses_controller.rb
@@ -0,0 +1,4 @@
+class OpusesController < ApplicationController
+  def index
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
+  validates :nickname, presence: true
+  validates :prefecture, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }
+  
+end
