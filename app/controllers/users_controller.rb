e9bb687f762b881385bb182ad1ddd6c8b0a09418
tairakanno <tairakannotaira@gmail.com>
2020-11-27 12:37:43 +0900

    アカウント削除修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     end  
   end
   def destroy
-    @user = User.find(params[:opus_id])
+    @user = User.find(params[:id])
     if @user.destroy
       redirect_to new_user_registration_path
     else
