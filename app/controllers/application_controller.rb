5fac4053e297a20616b578073396b5d959f4a596
tairakanno <tairakannotaira@gmail.com>
2020-12-18 17:33:47 +0900

    basic認証削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  # before_action :basic_auth
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :prefecture_id, :genre_id, :image, :profile])
